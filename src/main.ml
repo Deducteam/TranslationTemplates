451175573a92df2a86b6870e6b3b4bdc20fd220e
Thomas Traversié <thomas.traversie@gamil.com>
2024-09-04 15:49:22 +0200

    Theory embedding

--- src/main.ml
+++ src/main.ml
@@ -8,7 +8,8 @@ module Pp = Api.Pp.Default
 
 
 
-(* MORPHISM *)
+(* THEORY MORPHISM *)
+
 
 (* Morphism on ident id *)
 let morphism_ident id = 
@@ -110,12 +111,12 @@ let add1 x = x + 1;;
 let add2 x = x + 2;;
 
 (* Return an application between an option term ty 
-   (whose De Bruijn indices have been incremented twice) 
-   and a term u *)
-let app_option ty u =
+   (whose De Bruijn indices have been incremented twice),
+   a term u and a list l *)
+let app_option ty u l =
   match ty with
   | None -> None
-  | Some t -> Some (T.mk_App (op_var t add2 0) u [])
+  | Some t -> Some (T.mk_App (op_var t add2 0) u l)
 ;;
 
 (* Logical relation on ident id *)
@@ -152,7 +153,7 @@ let rec relation_term v arg =
         (morphism2_term a) 
         (T.mk_Pi lo (relation_ident id) 
           (T.mk_App (op_var (relation_term a None) add1 0) (T.mk_DB lo (morphism_ident id) 0) []) 
-          (relation_term b (app_option arg (T.mk_DB lo (morphism_ident id) 1))))
+          (relation_term b (app_option arg (T.mk_DB lo (morphism_ident id) 1) [])))
     else 
       T.mk_Lam lo (B.mk_ident "f") 
         (Some (morphism2_term v))
@@ -203,6 +204,144 @@ let relation_entry e =
   | DTree (_, _, _) -> []
   | Name (_, _) -> []
   | Require (lo, mid) -> [ Require (lo, morphism_mident mid) ]
+;;
+
+
+
+
+(* THEORY EMBEDDING *)
+
+
+(* Embedding morphism on ident id *)
+let m_ident id = 
+  (if B.ident_eq id B.dmark then 
+    id
+  else 
+    B.mk_ident (String.concat "" [B.string_of_ident id ; "_mu"]))
+;;
+
+(* Embedding relation on ident id *)
+let r_ident id = 
+  (if B.ident_eq id B.dmark then 
+    id
+  else 
+    B.mk_ident (String.concat "" [B.string_of_ident id ; "_rho"]))
+;;
+
+(* Embedding on mident id *)
+let mr_mident _ = 
+  B.mk_mident "module_to_erase"
+;;
+
+(* Embedding morphism on term v *)
+let rec m_term v = 
+  match v with
+  | T.App (t, u, l) -> 
+    T.mk_App 
+      (m_term t) 
+      (m_term u) 
+      (List.cons (r_term u None) (List.flatten (List.map (fun w -> [m_term w; r_term w None]) l)))
+  | Lam (lo, id, None, t) -> 
+    T.mk_Lam lo (m_ident id) None
+      (T.mk_Lam lo (r_ident id) None
+        (m_term t))
+  | Lam (lo, id, Some u, t) -> 
+    T.mk_Lam lo (m_ident id) 
+      (Some (m_term u)) 
+      (T.mk_Lam lo (r_ident id) 
+        (Some (T.mk_App 
+          (op_var (r_term u None) add1 0)
+          (T.mk_DB lo (m_ident id) 0) [])) 
+        (m_term t))
+  | Pi (lo, id, a, b) -> 
+    T.mk_Pi lo (m_ident id) 
+        (m_term a) 
+        (T.mk_Pi lo (r_ident id) 
+          (T.mk_App (op_var (r_term a None) add1 0) (T.mk_DB lo (m_ident id) 0) []) 
+          (m_term b))
+  | Kind -> v
+  | Type _ -> v
+  | DB (lo, id, n) -> T.mk_DB lo (m_ident id) (2* n + 1)
+  | Const (lo, n) -> T.mk_Const lo (B.mk_name (mr_mident (B.md n)) (m_ident (B.id n)))
+(* Embedding relation on term v *)
+and r_term v arg = 
+  match v with
+  | T.App (t, u, l) -> 
+    T.mk_App 
+      (r_term t None) 
+      (m_term u) 
+      (List.cons (r_term u None) (List.flatten (List.map (fun w -> [m_term w; r_term w None]) l)))
+  | Lam (lo, id, None, t) -> 
+    T.mk_Lam lo (m_ident id) None
+      (T.mk_Lam lo (r_ident id) None
+        (r_term t None))
+  | Lam (lo, id, Some u, t) -> 
+    T.mk_Lam lo (m_ident id) 
+      (Some (m_term u)) 
+      (T.mk_Lam lo (r_ident id) 
+        (Some (T.mk_App 
+          (op_var (r_term u None) add1 0)
+          (T.mk_DB lo (m_ident id) 0) [])) 
+        (r_term t None))
+  | Pi (lo, id, a, b) -> 
+    (if is_kind b then 
+      T.mk_Pi lo (m_ident id) 
+        (m_term a) 
+        (T.mk_Pi lo (r_ident id) 
+          (T.mk_App (op_var (r_term a None) add1 0) (T.mk_DB lo (m_ident id) 0) []) 
+          (r_term b (app_option arg (T.mk_DB lo (m_ident id) 1) [T.mk_DB lo (m_ident id) 0])))
+    else 
+      T.mk_Lam lo (B.mk_ident "f") 
+        (Some (m_term v))
+        (T.mk_Pi lo (m_ident id) 
+          (op_var (m_term a) add1 0) 
+          (T.mk_Pi lo (r_ident id) 
+            (T.mk_App (op_var (r_term a None) add2 0) (T.mk_DB lo (m_ident id) 0) []) 
+            (T.mk_App 
+              (op_var (r_term b None) add1 2)
+              (T.mk_App (T.mk_DB lo (B.mk_ident "f") 2) (T.mk_DB lo (m_ident id) 1) [(T.mk_DB lo (r_ident id) 0)]) 
+              []))))
+  | Kind -> v
+  | Type lo -> 
+    (match arg with 
+    | None -> v (* Do not happen *)
+    | Some t -> T.mk_Pi lo B.dmark t v)
+  | DB (lo, id, n) -> T.mk_DB lo (r_ident id) (n * 2)
+  | Const (lo, n) -> T.mk_Const lo (B.mk_name (mr_mident (B.md n)) (r_ident (B.id n)))
+;;
+
+
+(* Embedding on entry e *)
+let mr_entry e = 
+  let mid = B.mk_mident "module_to_erase" in 
+  let todo lo = T.mk_Const lo (B.mk_name (B.mk_mident "todo") (B.mk_ident "TODO")) in
+  match e with
+  | E.Decl (lo, id, s1, _, t) -> 
+    (if is_kind t then
+      [ E.Def (lo, m_ident id, s1, false, Some (m_term (explicit_var t)), todo lo) ;
+        E.Def (lo, r_ident id, s1, false, Some (r_term (explicit_var t) (Some (T.mk_Const lo (B.mk_name mid (m_ident id))))), todo lo) ]
+    else 
+      [ E.Def (lo, m_ident id, s1, false, Some (m_term (explicit_var t)), todo lo) ;
+        E.Def (lo, r_ident id, s1, false, Some (T.mk_App (r_term (explicit_var t) None) (T.mk_Const lo (B.mk_name mid (m_ident id))) []), todo lo) ])
+  | Def (lo, id, s, io, None, t) -> 
+    [ E.Def (lo, m_ident id, s, io, None, m_term (explicit_var t)) ; 
+        E.Def (lo, r_ident id, s, io, None, r_term (explicit_var t) None) ]
+  | Def (lo, id, s, io, Some u, t) -> 
+    (if is_kind u then
+      [ E.Def (lo, m_ident id, s, io, Some (m_term (explicit_var u)), m_term (explicit_var t)) ; 
+        E.Def (lo, r_ident id, s, io, Some (r_term (explicit_var u) (Some (T.mk_Const lo (B.mk_name mid (m_ident id))))), r_term (explicit_var t) None) ]
+    else 
+      [ E.Def (lo, m_ident id, s, io, Some (m_term (explicit_var u)), m_term (explicit_var t)) ; 
+        E.Def (lo, r_ident id, s, io, Some (T.mk_App (r_term (explicit_var u) None) (T.mk_Const lo (B.mk_name mid (m_ident id))) []), r_term (explicit_var t) None) ])
+  | Rules (_, _) -> [] 
+  | Eval (_, _, _) -> []
+  | Check (_, _, _, _) -> []
+  | Infer (_, _, _) -> []
+  | Print (_, _) -> []
+  | DTree (_, _, _) -> []
+  | Name (_, _) -> []
+  | Require (lo, mid) -> [ Require (lo, mr_mident mid) ]
+;;
 
 
 
@@ -225,14 +364,13 @@ let () =
     | None -> Format.printf "[Error] No file given for the target theory\n"
     | Some _ -> 
       (let parsed_file = P.(parse (input_from_file (String.concat "" [s ; ".dk"]))) in
-      (* print_string (String.concat "" ["#REQUIRE " ; t ; ".\n\n"]) ; *)
       match !template with
       | None -> Format.printf "[Error] No translation template given\n"
       | Some trans when (String.equal trans "morphism") -> 
         List.iter (fun e -> List.iter (fun l -> Format.printf "%a" Pp.print_entry l) (morphism_entry e)) parsed_file
       | Some trans when (String.equal trans "relation") -> 
         List.iter (fun e -> List.iter (fun l -> Format.printf "%a" Pp.print_entry l) (relation_entry e)) parsed_file
-      | Some trans when (String.equal trans "embedding") -> Format.printf "Not defined\n"
+      | Some trans when (String.equal trans "embedding") -> 
+        List.iter (fun e -> List.iter (fun l -> Format.printf "%a" Pp.print_entry l) (mr_entry e)) parsed_file
       | _ -> Format.printf "[Error] The translation template given does not exist\n"))
-      
 ;;
